822f319bd56c6843915cd14416ab41b6192d8332
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-11-30 19:46:07 -0500

    test written regarded to the faqvorite article repository

--- core/data/src/test/java/com/mahshad/data/DefaultFavoriteArticleRepositoryTest.kt
+++ core/data/src/test/java/com/mahshad/data/DefaultFavoriteArticleRepositoryTest.kt
@@ -1,22 +1,50 @@
 package com.mahshad.data
 
+import app.cash.turbine.test
+import com.mahshad.data.repository.DefaultFavoriteArticleRepository
 import com.mahshad.data.repository.FavoriteArticleRepository
+import com.mahshad.database.FavoriteArticleEntity
+import com.mahshad.database.toFavoriteArticle
 import com.mahshad.datasource.localdatasource.ArticleDataSource
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runTest
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
 class DefaultFavoriteArticleRepositoryTest {
     private lateinit var favoriteArticleRepository: FavoriteArticleRepository
-    @Mockk
+
+    @MockK
     private lateinit var articleDataSource: ArticleDataSource
 
     @Before
     fun setUp() {
-
+        MockKAnnotations.init(this)
+        favoriteArticleRepository = DefaultFavoriteArticleRepository(articleDataSource)
     }
 
     @Test
+    fun `getArticles_whenFavoritesExist_emitsConvertedArticles`() = runTest {
+        // Given
+        coEvery { articleDataSource.getArticles() } returns flow {
+            emit(listOf(FavoriteArticleEntity.DEFAULT))
+        }
+        // When
+        val articles = favoriteArticleRepository.getArticles()
+        //Then
+        articles.test {
+            assertEquals(
+                listOf(FavoriteArticleEntity.DEFAULT.toFavoriteArticle()),
+                awaitItem()
+            )
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
 
     @After
     fun tearDown() {
