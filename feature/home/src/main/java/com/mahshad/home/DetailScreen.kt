3fb401d4f881847339b0b9f61107642846fcd883
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-12-12 19:54:29 -0500

    the card for the bookmark view has changed

--- feature/home/src/main/java/com/mahshad/home/DetailScreen.kt
+++ feature/home/src/main/java/com/mahshad/home/DetailScreen.kt
@@ -2,7 +2,6 @@ package com.mahshad.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,15 +12,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells.Adaptive
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,51 +51,42 @@ fun DetailScreen(subject: String, viewModel: HomeScreenViewModel) {
 
 @Composable
 fun DetailCard(article: Article, onBookMarkClicked: (Article) -> Unit) {
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
     ) {
-//        val iconTint = LocalTintTheme.current.iconTint
-        Row(
-            modifier = Modifier.fillMaxHeight(0.2F),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                modifier = Modifier
-                    .weight(0.2F)
-                    .fillMaxHeight(1.0F)
-                    .clickable(true, onClick = { onBookMarkClicked(article) }),
-                painter = painterResource(id = R.drawable.bookmark_svgrepo_com),
-                colorFilter = ColorFilter.tint(
-                    color = if (article.isLiked) Color.Red
-                    else Color.Black
-                ),
-                contentDescription = null,
-            )
+        Column(modifier = Modifier.padding(16.dp)) {
+            Spacer(modifier = Modifier.height(12.dp))
+            Row {
+                Text(
+                    article.title,
+                    modifier = Modifier.fillMaxWidth((.8f)),
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Image(
+                    modifier = Modifier
+                        .weight(0.2F)
+                        .fillMaxHeight(1.0F)
+                        .clickable(true, onClick = { onBookMarkClicked(article) }),
+                    painter = painterResource(id = R.drawable.bookmark_svgrepo_com),
+                    colorFilter = ColorFilter.tint(
+                        color = if (article.isLiked) Color.Red
+                        else Color.Black
+                    ),
+                    contentDescription = null,
+                )
+            }
+            Spacer(modifier = Modifier.height(12.dp))
             Text(
                 modifier = Modifier
-                    .weight(0.8F)
-                    .fillMaxHeight(1.0F),
-                text = article.title,
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.8F),
+                text = article.content,
                 textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.bodyMedium,
             )
+            Spacer(modifier = Modifier.height(12.dp))
         }
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.8F),
-            text = article.content,
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.bodyMedium,
-        )
     }
 }
 
