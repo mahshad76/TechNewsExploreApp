4fe67d1708a73bac56f250a16f4ad6728341e27a
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-12-24 16:11:34 -0500

    the data module is imported by the interests module

--- feature/Interests/build.gradle.kts
+++ feature/Interests/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
 dependencies {
     implementation(project(":core:ui"))
+    implementation(project(":core:data"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- feature/Interests/src/main/java/com/mahshad/interests/InterestsScreenViewModel.kt
+++ feature/Interests/src/main/java/com/mahshad/interests/InterestsScreenViewModel.kt
@@ -1,13 +1,15 @@
 package com.mahshad.interests
 
 import androidx.lifecycle.ViewModel
+import com.mahshad.data.repository.UserDataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class InterestsScreenViewModel @Inject constructor() : ViewModel() {
+class InterestsScreenViewModel @Inject constructor(private val userDataRepository: UserDataRepository) :
+    ViewModel() {
     private val _checkedStateFlow: MutableStateFlow<List<String>> = MutableStateFlow(emptyList())
     val checkedStateFlow = _checkedStateFlow.asStateFlow()
     fun updateCheckedState() {
