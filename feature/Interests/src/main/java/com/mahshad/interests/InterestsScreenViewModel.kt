b29f041650a4db123abc2eea1f94361f95ca9d11
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-12-24 16:21:42 -0500

    view model for the interests screen is written

--- feature/Interests/src/main/java/com/mahshad/interests/InterestsScreenViewModel.kt
+++ feature/Interests/src/main/java/com/mahshad/interests/InterestsScreenViewModel.kt
@@ -1,22 +1,29 @@
 package com.mahshad.interests
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mahshad.data.repository.UserDataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class InterestsScreenViewModel @Inject constructor(private val userDataRepository: UserDataRepository) :
     ViewModel() {
-    private val _checkedStateFlow: MutableStateFlow<List<String>> = MutableStateFlow(emptyList())
-    val checkedStateFlow = _checkedStateFlow.asStateFlow()
-    fun updateCheckedState() {
-        // if
-    }
 
-    suspend fun getCheckedItems(): List<String> {
-        return emptyList()
+    val checkedStateFlow: StateFlow<Set<String>> = userDataRepository.getUserData()
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5_000),
+            emptySet()
+        )
+
+    fun updateCheckedState(favoriteTopics: Set<String>) {
+        viewModelScope.launch {
+            userDataRepository.postUserData(favoriteTopics)
+        }
     }
 }
