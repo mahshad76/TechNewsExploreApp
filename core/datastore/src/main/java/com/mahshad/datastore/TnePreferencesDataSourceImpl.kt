9b0166bc866e8416223ed0bc3798b5416b527196
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-12-23 22:18:05 -0500

    datastore instance is injected into the tne preference datastore class

--- core/datastore/src/main/java/com/mahshad/datastore/TnePreferencesDataSourceImpl.kt
+++ core/datastore/src/main/java/com/mahshad/datastore/TnePreferencesDataSourceImpl.kt
@@ -1,11 +1,16 @@
 package com.mahshad.datastore
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class TnePreferencesDataSourceImpl @Inject constructor() :
+class TnePreferencesDataSourceImpl @Inject constructor(
+    private val preferenceDataStore:
+    DataStore<Preferences>
+) :
     TnePreferencesDataSource {
     override fun getUserData(): Flow<Set<String>> {
         TODO("Not yet implemented")
