d1f6de567e762c0a6aa3fc36d8b3cfd5a60b3f87
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-12-25 16:54:12 -0500

    SwipeableTabScreen is added to the ui components module, it has a tab and a pager, it will be used in the home nodule to swipe between all the news and favorite ones

---
+++ core/ui/src/main/java/com/mahshad/ui/components/SwipeableTabScreen.kt
@@ -0,0 +1,43 @@
+package com.mahshad.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
+
+@Composable
+fun SwipeableTabScreen(
+    tabs: List<String>,
+    pageNavigator: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val pagerState = rememberPagerState(pageCount = { tabs.size })
+    val scope = rememberCoroutineScope()
+    Column(modifier = modifier.fillMaxSize()) {
+        TabRow(selectedTabIndex = pagerState.currentPage, modifier = Modifier.fillMaxWidth()) {
+            tabs.forEachIndexed { index, title ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        scope.launch { pagerState.animateScrollToPage(index) }
+                    },
+                    text = { Text(title) }
+                )
+            }
+        }
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxSize()
+        ) { page ->
+            pageNavigator(page)
+        }
+    }
+}
