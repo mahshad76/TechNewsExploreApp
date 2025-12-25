1b423e15a63a426eea720d37a8994bb5cc2b1c03
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-12-24 20:42:10 -0500

    using the statein instead of manual collecting in the interests screen viewmodel

--- feature/Interests/src/main/java/com/mahshad/interests/InterestsScreenViewModel.kt
+++ feature/Interests/src/main/java/com/mahshad/interests/InterestsScreenViewModel.kt
@@ -6,30 +6,27 @@ import com.mahshad.data.repository.UserDataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class InterestsScreenViewModel @Inject constructor(private val userDataRepository: UserDataRepository) :
     ViewModel() {
-
-    val checkedStateFlow: StateFlow<Set<String>> = userDataRepository.getUserData()
-        .stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(5_000),
-            emptySet()
-        )
-
-    fun updateCheckedState(favoriteTopics: Set<String>) {
-        viewModelScope.launch {
-            userDataRepository.postUserData(favoriteTopics)
-        }
-    }
+    val interestingTopicStateFlow: StateFlow<UiState<Set<String>>> =
+        userDataRepository.getUserData()
+            .map { UiState.Success(it) }
+            .catch { UiState.Error(it) }
+            .stateIn(
+                viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                UiState.Loading
+            )
 }
 
-sealed interface UiState {
-    data class Success(val favoriteTopics: Set<String>) : UiState
-    data class Error(val error: Throwable) : UiState
-    data object Loading : UiState
+sealed interface UiState<out T> {
+    data class Success(val favoriteTopics: Set<String>) : UiState<Set<String>>
+    data class Error(val error: Throwable) : UiState<Nothing>
+    data object Loading : UiState<Nothing>
 }
