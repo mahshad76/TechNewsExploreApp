8da087f87f22fd7ca6f76518482fdbf4bb8959f8
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-12-27 14:27:55 -0500

    using flatmaplatest and flatmapmerge for the favorite news screen

--- feature/home/src/main/java/com/mahshad/home/FavoriteNewsScreenViewModel.kt
+++ feature/home/src/main/java/com/mahshad/home/FavoriteNewsScreenViewModel.kt
@@ -6,6 +6,11 @@ import com.mahshad.data.repository.ArticleRepository
 import com.mahshad.data.repository.FavoriteArticleRepository
 import com.mahshad.data.repository.UserDataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,5 +20,9 @@ class FavoriteNewsScreenViewModel @Inject constructor(
     private val favoriteArticleRepository: FavoriteArticleRepository,
     private val userDataRepository: UserDataRepository
 ) : ViewModel() {
-
+    lateinit var b: MutableList<Flow<List<String>>>
+    val a = userDataRepository.getUserData().flatMapLatest { favoriteTopics: Set<String> ->
+        val a: Flow<String> = favoriteTopics.asFlow()
+        a.flatMapMerge { string -> flowOf(string) }
+    }
 }
