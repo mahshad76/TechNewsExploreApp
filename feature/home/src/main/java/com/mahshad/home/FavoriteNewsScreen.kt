50164e7c1fcc3aee558f5c71653a54d967cd130a
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-12-27 16:20:54 -0500

    favorite suggestions are shown correctly

--- feature/home/src/main/java/com/mahshad/home/FavoriteNewsScreen.kt
+++ feature/home/src/main/java/com/mahshad/home/FavoriteNewsScreen.kt
@@ -1,9 +1,56 @@
 package com.mahshad.home
 
-import androidx.compose.material3.Text
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.mahshad.ui.NewsFeed
+import com.mahshad.ui.components.TneLoadingWheel
 
 @Composable
-fun FavoriteNewsScreen() {
-    Text("Liked")
+fun FavoriteNewsScreen(viewModel: FavoriteNewsScreenViewModel = hiltViewModel()) {
+    val searchSuggestionState = viewModel.searchSuggestions.collectAsStateWithLifecycle()
+    val searchQuery = viewModel.searchQueryStateFlow.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 12.dp)
+    ) {
+        Spacer(Modifier.windowInsetsTopHeight(WindowInsets.safeDrawing))
+        HomeSearchBar(
+            modifier = Modifier.padding(bottom = 4.dp),
+            searchQuery.value,
+            { viewModel.updateSearchStateFlow(it) },
+            {
+                //navigateFromHomeToDetail(searchQuery.value)
+            })
+        when (searchSuggestionState.value) {
+            is NewsFeed.Loading -> TneLoadingWheel()
+            is NewsFeed.Error -> Toast.makeText(
+                context, "Something went wrong",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            is NewsFeed.Successful -> {
+                SearchSuggestionsBox(
+                    (searchSuggestionState.value as NewsFeed.Successful).news.map { it.title }) {
+                    viewModel.updateSearchStateFlow(it)
+                }
+            }
+        }
+        Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.safeDrawing))
+    }
 }
