070e94ec4fa8b9b85b6cd89f9a315e8b3ce06083
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-12-29 13:42:07 -0500

    bookmark icon is aligned to the center end of the search suggestion card with an appropriate size and padding

--- feature/home/src/main/java/com/mahshad/home/SearchSuggestionsBox.kt
+++ feature/home/src/main/java/com/mahshad/home/SearchSuggestionsBox.kt
@@ -1,8 +1,10 @@
 package com.mahshad.home
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,6 +15,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +24,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 
@@ -42,7 +44,8 @@ fun SearchSuggestionsBox(
                     ArticleCard(
                         title = suggestions[index].first,
                         url = suggestions[index].second,
-                        onClick = { onSuggestionClick(suggestions[index].first) }
+                        onClick = { onSuggestionClick(suggestions[index].first) },
+                        icon = icon
                     )
             }
         }
@@ -53,7 +56,8 @@ fun SearchSuggestionsBox(
 fun ArticleCard(
     title: String,
     url: String,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    icon: Painter
 ) {
     Card(
         modifier = Modifier
@@ -80,14 +84,30 @@ fun ArticleCard(
                     .take(10)
                     .joinToString(" "),
                 style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.align(Alignment.CenterVertically)
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .weight(3.0F)
             )
+            Box(
+                modifier = Modifier
+                    .weight(1.0F)
+                    .fillMaxHeight()
+                    .padding(end = 7.dp),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                Icon(
+                    icon,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(20.dp)
+                )
+            }
         }
     }
 }
 
-@Preview
-@Composable
-fun ArticleCardPreview() {
-    ArticleCard("news", "url", {})
-}
+//@Preview
+//@Composable
+//fun ArticleCardPreview() {
+//    ArticleCard("news", "url", {})
+//}
