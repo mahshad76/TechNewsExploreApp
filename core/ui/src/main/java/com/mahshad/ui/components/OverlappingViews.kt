ec8c23cb6ac4806fa5ec9a1ef1331f6df76697d7
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-12-31 20:16:02 -0500

    creating the loginformcontent and pass it as a parameter to the overlapping screen

--- core/ui/src/main/java/com/mahshad/ui/components/OverlappingViews.kt
+++ core/ui/src/main/java/com/mahshad/ui/components/OverlappingViews.kt
@@ -3,6 +3,8 @@ package com.mahshad.ui.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
@@ -17,10 +19,14 @@ fun OverlappingViews(
     content: @Composable () -> Unit
 ) {
     Column(modifier = Modifier.fillMaxSize()) {
-        Card(shape = backgroundShape, modifier = backgroundModifier) {
+        Card(shape = backgroundShape, modifier = backgroundModifier.weight(30.0F)) {
             background()
         }
-        Card(shape = contentShape, modifier = contentModifier) {
+        Card(
+            shape = contentShape,
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+            modifier = contentModifier.weight(70.0F)
+        ) {
             content()
         }
     }
--- feature/welcome/src/main/java/com/mahshad/welcome/login/LoginScreen.kt
+++ feature/welcome/src/main/java/com/mahshad/welcome/login/LoginScreen.kt
@@ -1,3 +1,4 @@
+import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
@@ -6,7 +7,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -15,9 +15,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -40,6 +37,7 @@ import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mahshad.ui.components.ModifiedButton
 import com.mahshad.ui.components.ModifiedTextFiled
+import com.mahshad.ui.components.OverlappingViews
 import com.mahshad.ui.icons.TneIcons.AuthenticationBackground
 import com.mahshad.welcome.R
 import com.mahshad.welcome.login.LoginScreenViewModel
@@ -60,183 +58,204 @@ fun LoginScreen(
     val isEnabledState = viewModel.isEnabled.collectAsStateWithLifecycle()
     val loginStatusState = viewModel.loginStatusFlow.collectAsStateWithLifecycle()
     val context = LocalContext.current
-    Column(modifier = Modifier.fillMaxSize()) {
-        Card(
-            shape = RoundedCornerShape(0.dp),
+    val content: @Composable () -> Unit = {
+        LoginFormContent(
+            onNavigateToHome = onNavigateToHome,
+            onNavigateToSignUp = onNavigateToSignUp,
+            updateFlow = { update: String, type: Boolean -> viewModel.updateFlow(update, type) },
+            login = { username: String, password: String -> viewModel.login(username, password) },
+            usernameState = usernameState.value,
+            passwordState = passwordState.value,
+            isEnabledState = isEnabledState.value,
+            loginStatusState = loginStatusState.value,
+            context = context
+        )
+    }
+    val background: @Composable () -> Unit = {
+        Image(
+            painter = painterResource(id = AuthenticationBackground),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+    }
+
+    OverlappingViews(
+        backgroundModifier = Modifier.fillMaxWidth(),
+        contentModifier = Modifier
+            .fillMaxWidth()
+            .offset(y = -100.dp),
+        backgroundShape = RoundedCornerShape(0.dp),
+        contentShape = RoundedCornerShape(24.dp),
+        background = background,
+        content = content
+    )
+}
+
+@Composable
+private fun LoginFormContent(
+    onNavigateToHome: (String) -> Unit,
+    onNavigateToSignUp: () -> Unit,
+    updateFlow: (String, Boolean) -> Unit,
+    login: (String, String) -> Unit,
+    usernameState: String,
+    passwordState: String,
+    isEnabledState: Boolean,
+    loginStatusState: Boolean,
+    context: Context,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Login",
+            fontFamily = FontFamily(Font(R.font.poppins_bold)),
+            fontWeight = FontWeight.Bold,
+            fontSize = 40.sp,
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(30.0F),
-        ) {
-            Image(
-                painter = painterResource(id = AuthenticationBackground),
-                contentDescription = null,
-                contentScale = ContentScale.Crop
-            )
-        }
+                .padding(bottom = 20.dp)
+        )
 
-        Card(
-            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
-            elevation = CardDefaults.cardElevation(defaultElevation = 0.dp),
-            shape = RoundedCornerShape(24.dp),
+        ModifiedTextFiled(
+            value = usernameState,
+            onValueChanged = { updateFlow(it, true) },
+            placeHolder = { Text("Email", color = Color.Gray) },
+            cornerRadius = 0,
+            color = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(70.0F)
-                .offset(y = -100.dp)
-        ) {
-            Column(
-                modifier = modifier
-                    .padding(24.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "Login",
-                    fontFamily = FontFamily(Font(R.font.poppins_bold)),
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 40.sp,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 20.dp)
-                )
-
-                ModifiedTextFiled(
-                    value = usernameState.value,
-                    onValueChanged = { viewModel.updateFlow(it, true) },
-                    placeHolder = { Text("Email", color = Color.Gray) },
-                    cornerRadius = 0,
-                    color = TextFieldDefaults.colors(
-                        focusedContainerColor = Color.Transparent,
-                        unfocusedContainerColor = Color.Transparent,
-                        disabledContainerColor = Color.Transparent,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent
-                    ),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .drawBehind {
-                            drawLine(
-                                color = Color.LightGray,
-                                start = Offset(0f, size.height),
-                                end = Offset(size.width, size.height),
-                                strokeWidth = 1.dp.toPx()
-                            )
-                        }
-                )
-
-                Spacer(Modifier.height(20.dp))
-
-                ModifiedTextFiled(
-                    value = passwordState.value,
-                    onValueChanged = { viewModel.updateFlow(it, false) },
-                    placeHolder = { Text("Password", color = Color.Gray) },
-                    cornerRadius = 0,
-                    color = TextFieldDefaults.colors(
-                        focusedContainerColor = Color.Transparent,
-                        unfocusedContainerColor = Color.Transparent,
-                        disabledContainerColor = Color.Transparent,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent
-                    ),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .drawBehind {
-                            drawLine(
-                                color = Color.LightGray,
-                                start = Offset(0f, size.height),
-                                end = Offset(size.width, size.height),
-                                strokeWidth = 1.dp.toPx()
-                            )
-                        }
-                )
-
-                Text(
-                    "Forgot password?",
-                    fontSize = 14.sp,
-                    color = colorResource(R.color.blue),
-                    modifier = Modifier
-                        .padding(vertical = 15.dp)
-                        .align(Alignment.End)
-                        .clickable { /* action */ }
-                )
-
-                ModifiedButton(
-                    content = {
-                        Text("Login", fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
-                    },
-                    onClick = {
-                        viewModel.login(usernameState.value, passwordState.value)
-                        if (loginStatusState.value) {
-                            onNavigateToHome(usernameState.value)
-                        } else {
-                            Toast.makeText(
-                                context,
-                                "Wrong username or password",
-                                Toast.LENGTH_SHORT
-                            )
-                                .show()
-                        }
-                    },
-                    shape = RoundedCornerShape(10.dp),
-                    buttonColors = ButtonDefaults.buttonColors(
-                        containerColor = colorResource(R.color.blue),
-                        contentColor = Color.White
-                    ),
-                    borderStroke = BorderStroke(0.5.dp, Color.LightGray),
-                    enabled = isEnabledState.value,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(55.dp)
-                )
-
-                Text(
-                    "Or login with",
-                    fontSize = 14.sp,
-                    color = Color.Gray,
-                    modifier = Modifier.padding(vertical = 25.dp)
-                )
-
-                ModifiedButton(
-                    content = {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            Image(
-                                painter = painterResource(R.drawable.google),
-                                contentDescription = "google icon",
-                                modifier = Modifier.size(24.dp),
-                            )
-                            Spacer(Modifier.width(10.dp))
-                            Text("Google", fontWeight = FontWeight.Bold)
-                        }
-                    },
-                    onClick = {},
-                    shape = RoundedCornerShape(12.dp),
-                    buttonColors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Transparent,
-                        contentColor = Color.Black
-                    ),
-                    borderStroke = BorderStroke(0.5.dp, Color.LightGray),
-                    enabled = true,
-                    modifier = Modifier
-                        .width(180.dp)
-                        .height(50.dp)
-                )
-
-                Spacer(Modifier.weight(1f))
-
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(bottom = 20.dp)
-                ) {
-                    Text("New here?", fontSize = 15.sp, color = Color.Gray)
-                    Spacer(Modifier.width(5.dp))
-                    Text(
-                        text = "Register",
-                        fontSize = 15.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = colorResource(R.color.blue),
-                        modifier = Modifier.clickable { onNavigateToSignUp() }
+                .drawBehind {
+                    drawLine(
+                        color = Color.LightGray,
+                        start = Offset(0f, size.height),
+                        end = Offset(size.width, size.height),
+                        strokeWidth = 1.dp.toPx()
                     )
                 }
-            }
+        )
+
+        Spacer(Modifier.height(20.dp))
+
+        ModifiedTextFiled(
+            value = passwordState,
+            onValueChanged = { updateFlow(it, false) },
+            placeHolder = { Text("Password", color = Color.Gray) },
+            cornerRadius = 0,
+            color = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .drawBehind {
+                    drawLine(
+                        color = Color.LightGray,
+                        start = Offset(0f, size.height),
+                        end = Offset(size.width, size.height),
+                        strokeWidth = 1.dp.toPx()
+                    )
+                }
+        )
+
+        Text(
+            "Forgot password?",
+            fontSize = 14.sp,
+            color = colorResource(R.color.blue),
+            modifier = Modifier
+                .padding(vertical = 15.dp)
+                .align(Alignment.End)
+                .clickable { /* action */ }
+        )
+
+        ModifiedButton(
+            content = {
+                Text("Login", fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
+            },
+            onClick = {
+                login(usernameState, passwordState)
+                if (loginStatusState) {
+                    onNavigateToHome(usernameState)
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Wrong username or password",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            },
+            shape = RoundedCornerShape(10.dp),
+            buttonColors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(R.color.blue),
+                contentColor = Color.White
+            ),
+            borderStroke = BorderStroke(0.5.dp, Color.LightGray),
+            enabled = isEnabledState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp)
+        )
+
+        Text(
+            "Or login with",
+            fontSize = 14.sp,
+            color = Color.Gray,
+            modifier = Modifier.padding(vertical = 25.dp)
+        )
+
+        ModifiedButton(
+            content = {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(
+                        painter = painterResource(R.drawable.google),
+                        contentDescription = "google icon",
+                        modifier = Modifier.size(24.dp),
+                    )
+                    Spacer(Modifier.width(10.dp))
+                    Text("Google", fontWeight = FontWeight.Bold)
+                }
+            },
+            onClick = {},
+            shape = RoundedCornerShape(12.dp),
+            buttonColors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Black
+            ),
+            borderStroke = BorderStroke(0.5.dp, Color.LightGray),
+            enabled = true,
+            modifier = Modifier
+                .width(180.dp)
+                .height(50.dp)
+        )
+
+        Spacer(Modifier.weight(1f))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 20.dp)
+        ) {
+            Text("New here?", fontSize = 15.sp, color = Color.Gray)
+            Spacer(Modifier.width(5.dp))
+            Text(
+                text = "Register",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.blue),
+                modifier = Modifier.clickable { onNavigateToSignUp() }
+            )
         }
     }
 }
