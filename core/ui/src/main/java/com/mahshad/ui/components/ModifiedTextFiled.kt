1e28aa69baff2af5d4f10dbf4f560f725be7f1f1
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2026-01-07 18:14:15 -0500

    a boolean parametr of isPassword is added to the modified text field to add the visibility modification functionality for the password text fields

--- core/ui/src/main/java/com/mahshad/ui/components/ModifiedTextFiled.kt
+++ core/ui/src/main/java/com/mahshad/ui/components/ModifiedTextFiled.kt
@@ -14,6 +14,7 @@ fun ModifiedTextFiled(
     placeHolder: @Composable () -> Unit,
     cornerRadius: Int,
     color: TextFieldColors,
+    isPassword: Boolean = false,
     modifier: Modifier
 ) {
     TextField(
